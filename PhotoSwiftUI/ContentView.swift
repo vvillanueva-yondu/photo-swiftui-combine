8ad4cabf65f04dd5bce56dc8f18d10166087bb50
vincevillanueva <vvillanueva@yondu.com>
2022-05-12 15:15:48 +0800

    Initial Commit

---
+++ PhotoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoSwiftUI
+//
+//  Created by Vince VIllanueva on 5/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoSwiftUI/PhotoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSwiftUIApp.swift
+//  PhotoSwiftUI
+//
+//  Created by Vince VIllanueva on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
